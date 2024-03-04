26dda470950199b5f4e47fc66bb217cea07922f7
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-03-05 02:48:13 +0530

    All neaded file uploaded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter , Routes , Route} from 'react-router-dom';
 import './App.css'
 import Startpg from './components/startpage';
 import NavBar from './components/navbar';
-
+import EventInfoCard from './components/EventInfoCard';
 function App() {
   
   return (
