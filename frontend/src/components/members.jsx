beb145123aadd7c9664cb8bf416b0c6da3e420b8
Om Patil <ompatil4749@gmail.com>
2024-03-29 23:33:30 +0530

    members section

---
+++ frontend/src/components/members.jsx
@@ -0,0 +1,99 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const membersData = [
+  {
+    name: "Om Patil",
+    img: `https://res.cloudinary.com/dpn9zrpa8/image/upload/v1711731345/images/Om_Patil_djppab.jpg`,
+    position: "Creatives",
+    link: "Link",
+  },
+  {
+    name: "Prajwal Sakunde",
+    img: "https://res.cloudinary.com/dpn9zrpa8/image/upload/v1711731341/images/Prajwal_Sakunde_mfulx6.jpg",
+    position: "Web ERP",
+    link: "Link",
+  },
+  {
+    name: "Rugved Kulkarni",
+    img: "https://res.cloudinary.com/dpn9zrpa8/image/upload/v1711731336/images/RugvedPhoto_ktxluq.jpg",
+    position: "Secretary",
+    link: "Link",
+  },
+  {
+    name: "Pruthvij Desai",
+    img: "https://res.cloudinary.com/dpn9zrpa8/image/upload/v1711731338/images/Pruthvij_hczpo2.jpg",
+    position: "Descipline",
+    link: "Link",
+  },
+];
+
+const members = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+        <Slider {...settings}>
+          {membersData.map((d) => (
+            <div className="bg-white h-[450px] text-black rounded-xl">
+              <div className="h-56 rounded-t-xl flex justify-center items-center ">
+                <img
+                  src={d.img}
+                  className="h-44 w-44 rounded-full"
+                  alt="image not loading"
+                />
+              </div>
+
+              <div className="flex flex-col justify-center items-center gap-4 p-4">
+                <p className="text-xl font-semibold">{d.name}</p>
+                <p>{d.position}</p>
+                <button className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">
+                  Link
+                </button>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default members;
--- frontend/src/components/startpage.jsx
+++ frontend/src/components/startpage.jsx
@@ -3,65 +3,74 @@ import { gsap } from "gsap";
 import { useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import SplitTextJS from "split-text-js";
-
+import Members from "./members";
 const Startpg = () => {
-    useLayoutEffect(() => {
-        const tl = gsap.timeline({ repeat: -1 });
+  useLayoutEffect(() => {
+    const tl = gsap.timeline({ repeat: -1 });
 
-        const titles = gsap.utils.toArray('.text-wrapper p');
+    const titles = gsap.utils.toArray(".text-wrapper p");
 
-        titles.forEach((title, index) => {
-            const spiltTitle = new SplitTextJS(title);
+    titles.forEach((title, index) => {
+      const spiltTitle = new SplitTextJS(title);
 
-            tl.from(spiltTitle.chars, {
-                opacity: 0,
-                y: 60,
-                rotateX: -90,
-                stagger: 0.02,
-                delay: index * 0.02
-            }, "<")
+      tl.from(
+        spiltTitle.chars,
+        {
+          opacity: 0,
+          y: 60,
+          rotateX: -90,
+          stagger: 0.02,
+          delay: index * 0.02,
+        },
+        "<"
+      ).to(
+        spiltTitle.chars,
+        {
+          opacity: 0,
+          y: -60,
+          rotateX: 90,
+          stagger: 0.02,
+        },
+        "<1"
+      );
+    });
 
-            .to(spiltTitle.chars, {
-                opacity: 0,
-                y: -60,
-                rotateX: 90,
-                stagger: 0.02
-            }, "<1");
-        });
+    return () => {
+      tl.kill(); // Cleanup animation timeline
+    };
+  }, []); // Empty dependency array ensures this effect runs only once after the initial render
 
-        return () => {
-            tl.kill(); // Cleanup animation timeline
-        };
-    }, []); // Empty dependency array ensures this effect runs only once after the initial render
+  return (
+    <main className="overflow-hidden">
+      <div className="p-1 sm:p-2 bg-gradient-to-r from-indigo-200 from-10%  via-sky-200 via-40% to-fuchsia-100 to-90% "></div>
+      <div className="relative ">
+        <div className="flex justify-center relative mt-5 items-center ">
+          <div className="w-full h-screen text-wrapper relative top-20 sm:top-40 items-center font-sans font-light text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-10xl ">
+            <Link>
+              <p>Debuggers' Club</p>
+              <p>Run by the student</p>
+              <p>for the students</p>
+              <p>of the student</p>
+            </Link>
+          </div>
+        </div>
+        <div className="flex justify-center relative w-full h-screen ">
+          <div className="absolute  text-center ">
+            <h2 className="text-3xl font-bold">Event</h2>
+            <p className="mt-2 text-lg">Event section content goes here.</p>
+          </div>
+        </div>
 
-    return (
-        <main className="overflow-hidden">
-            <div className="p-1 sm:p-2 bg-gradient-to-r from-indigo-200 from-10%  via-sky-200 via-40% to-fuchsia-100 to-90% ">
-                
-            </div>
-           <div className="relative ">
-            
-            <div className="flex justify-center relative mt-5 items-center ">
-                <div className="w-full h-screen text-wrapper relative top-20 sm:top-40 items-center font-sans font-light text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-10xl ">
-                <Link>
-                    <p>Debuggers' Club</p>
-                    <p>Run by the student</p>
-                    <p>for the students</p>
-                    <p>of the student</p>
-                </Link>
-                </div>
-            </div>
-            <div className="flex justify-center relative w-full h-screen ">
-
-            <div className="absolute  text-center ">
-                <h2 className="text-3xl font-bold">Event</h2>
-                <p className="mt-2 text-lg">Event section content goes here.</p>
-            </div>
-            </div>
-            </div> 
-
-        </main>
-    );
-}
+        <div className="flex justify-center relative w-full h-screen ">
+          <div className="absolute  text-center ">
+            <h2 className="text-3xl font-bold">Members</h2>
+            <p className="mt-2 text-lg">Members of the 2024 Debuggers Club.</p>
+          </div>
+          <Members />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Startpg;
