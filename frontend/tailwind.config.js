45c2149173d398be6dab39fb0f4b3aa10a89f16a
Prajwal Sakunde <prajwalsakunde1234@gmail.com>
2024-03-30 22:09:32 +0530

    new commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,22 @@
 // tailwind.config.js
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      height: {
-        128: "40rem",
-      },
-      screen: {
-        xs: "250px",
+      screen:{
+        'xs':'250px',
       },
       fontSize: {
-        "10xl": "10rem", // Custom text size
-        "11xl": "11rem", // Additional custom text size if needed
+        '10xl': '10rem', // Custom text size
+        '11xl': '11rem', // Additional custom text size if needed
         // Add more custom text sizes as needed
       },
+       
     },
   },
   plugins: [],
-};
+}
